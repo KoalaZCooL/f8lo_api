92203b629a6798ce48aba3969315ff150bf0df5a
Faza <fazafauzanadhiman@gmail.com>
2020-03-23 02:15:24 +0700

    heroku scheduler

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: vendor/bin/heroku-php-nginx public/
+web: vendor/bin/heroku-php-nginx -C nginx_app.conf public/
+scheduler: php -d memory_limit=512M artisan schedule:daemon
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SchedulerDaemon;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+use App\Models\Product;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+      SchedulerDaemon::class
     ];
 
     /**
@@ -24,6 +26,46 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+      $schedule->call(function () {
+        $products = Product::all(["url", "id"]);
+        foreach ($products as $prd) {
+          if($price = $this->fetchPrice($prd->url) ){
+            $prd->prices()->create([
+              "price" => $price,
+              "currency" => "IDR"
+            ]);
+          }
+        }
+      })->everyMinute();
+    }
+
+    /**
+     * method to fetch meta price from URL
+     */
+    protected function fetchPrice($url){
+      #fetch the product's web content
+      $res = file_get_contents($url);
+  
+      #extract the html head
+      $res = substr($res, 0, strpos($res, "</head") );
+  
+      $res = array_filter(
+        array_map(
+          #cleanup each data
+          function($v){return trim($v);},
+  
+          #split data by meta tag
+          explode("<meta", $res )
+        ),
+  
+        #pick only meta open graph prices
+        function($v){return 0===strpos($v,'property="product:price:amount');}
+      );
+  
+      foreach ($res as $v) {
+        $s = substr($v, strpos($v, 'content="')+9);
+        return substr($s, 0, strpos($s, '"'));
+      }
+      return null;
     }
 }
